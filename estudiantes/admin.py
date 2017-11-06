53fbaa1ab3d643bef99e6171255dc3b0f5116edb
Fernando <f-er1016@hotmail.com>
2017-11-06 13:16:01 -0600

    subir a python anywhere

--- estudiantes/admin.py
+++ estudiantes/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from estudiantes.models import Curso, CursoAdmin, Estudiante, EstudianteAdmin
+from estudiantes.models import Curso, CursoAdmin, Estudiante, EstudianteAdmin, Profesor, Aula
 # Register your models here.
 admin.site.register(Curso, CursoAdmin)
 admin.site.register(Estudiante, EstudianteAdmin)
+admin.site.register(Profesor)
+admin.site.register(Aula)
